28d969266c81a34c2af428e626fb24ad2c039aa6
Josh Nichols <josh@technicalpickles.com>
2008-09-26 18:21:30 -0400

    Whoops, typos.

--- lib/woulda/acts_as_paranoid.rb
+++ lib/woulda/acts_as_paranoid.rb
@@ -1,6 +1,6 @@
 require 'shoulda'
 require 'woulda/acts_as_paranoid/macros'
 
-Test::Unit::TestCase.class_eval
+Test::Unit::TestCase.class_eval do
   extend Woulda::ActsAsParanoid::Macros
 end
--- lib/woulda/attachment_fu.rb
+++ lib/woulda/attachment_fu.rb
@@ -1,6 +1,6 @@
 require 'shoulda'
 require 'woulda/attachment_fu/macros'
 
-Test::Unit::TestCase.class_eval
+Test::Unit::TestCase.class_eval do
   extend Woulda::AttachmentFu::Macros
 end
