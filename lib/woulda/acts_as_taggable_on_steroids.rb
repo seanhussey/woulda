e685e5b0be504daf6ed47d27329e71072b247b0a
Lee Irving <lee.irving@transcendit.co.uk>
2008-12-05 13:44:30 +0000

    missing /macro on Require File

--- lib/woulda/acts_as_taggable_on_steroids.rb
+++ lib/woulda/acts_as_taggable_on_steroids.rb
@@ -1,5 +1,5 @@
 require 'shoulda'
-require File.dirname(__FILE__) + '/acts_as_taggable_on_steroids'
+require File.dirname(__FILE__) + '/acts_as_taggable_on_steroids/macro'
 
 Test::Unit::TestCase.class_eval do
   extend Woulda::ActsAsTaggableOnSteroids::Macros
