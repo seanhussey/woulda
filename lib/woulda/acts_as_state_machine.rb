0a13ab0c0bca01c85d86a7cf289b4faa5e1ef7b9
Brian Cardarella <bcardarella@gmail.com>
2009-02-26 06:11:44 +0800

    I'm a retard
    Signed-off-by: Sean Hussey <sean@seanhussey.com>

--- lib/woulda/acts_as_state_machine.rb
+++ lib/woulda/acts_as_state_machine.rb
@@ -1,6 +1,6 @@
 require 'shoulda'
-require File.dirname(__FILE__) + '/acts_as_state_machine/macros_old' unless const_defined?(AASM)
-require File.dirname(__FILE__) + '/acts_as_state_machine/macros_new' if const_defined?(AASM)
+require File.dirname(__FILE__) + '/acts_as_state_machine/macros_old' unless defined?(AASM)
+require File.dirname(__FILE__) + '/acts_as_state_machine/macros_new' if defined?(AASM)
 
 Test::Unit::TestCase.class_eval do
   extend Woulda::ActsAsStateMachine::Macros
